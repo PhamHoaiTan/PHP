2f74b75756e9e9e57e37c91bb3e473a64a368fd4
David Pham <95225335+PhamHoaiTan@users.noreply.github.com>
2024-08-22 10:58:50 +0700

    update demo

---
+++ day1/demo2.php
@@ -0,0 +1,16 @@
+<?php
+$num1=7;
+$num2=7.7;
+$num3="nguyen van a";
+$isActive = true;
+//khai bao mang
+$myArray = array();
+//gan gia tri cho mang
+$myArray[] = "Van A";
+$myArray[] = "Van B";
+$myArray[] = "Van C";
+echo 'num1:' . $num1 .'<br>'; 
+echo 'num2:' . $num2 .'<br>';
+echo 'num3:' . $num3 .'<br>';
+echo 'Active:' . $isActive;
+?>
---
+++ day1/demo7.php
@@ -0,0 +1,37 @@
+<?php
+    // Lấy thông tin từ form với method get
+    if(isset($_POST["username"]))
+    {
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+        if($username == "aptech" && $password == "123")
+        {
+            echo "Login thành công";
+        }
+        else
+        {
+            echo "Login false";
+        }
+    }
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- Phải có biến name cho input -->
+    <!-- Method: get sẽ đưa lên thanh địa chỉ (default)-->
+     <!-- Method:post sẽ kh đưa lên  -->
+    <form Method="post">
+        <h1>Form</h1>
+        username : <input type="text" name="username"><br>
+        password <input type="password" name="password">
+        <input type="submit" value="login">
+    </form>
+</body>
+</html>
